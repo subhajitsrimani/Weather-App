0dd3d78cfd560e1a14d56996cc9229aaec1af79e
DozzerGeeky <d2b4mb@gmail.com>
2023-08-22 21:41:51 +0530

    day and night implement

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ search.addEventListener('click', () => {
 
     const APIKey = 'd1845658f92b31c64bd94f06f7188c9c';
     const city = document.querySelector('.search-box input').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`;
 
     if (city === '')
         return;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    fetch(url)
         .then(response => response.json())
         .then(json => {
-
+            const data = json;
+            const timestamp = data.dt;
+            const date = new Date(timestamp * 1000);
+            const hour = date.getHours();
             if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
@@ -24,7 +28,6 @@ search.addEventListener('click', () => {
                 error404.classList.add('fadeIn');
                 return;
             }
-
             error404.style.display = 'none';
             error404.classList.remove('fadeIn');
 
@@ -33,11 +36,15 @@ search.addEventListener('click', () => {
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
-
+            
             switch (json.weather[0].main) {
                 case 'Clear':
-                    image.src = 'images/clear.png';
-                    break;
+                    if (hour >= 6 && hour < 18) {
+                        image.src = "images/clear.png";
+                      } else {
+                        image.src = "images/moon.png";
+                      }
+                      break;
 
                 case 'Rain':
                     image.src = 'images/rain.png';
